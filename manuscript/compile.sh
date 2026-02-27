97392006a20c4df70282d4ce47ad73b909ac1e90
T. Latrille <thibault.latrille@ens-lyon.org>
2026-02-27 10:20:33 +0100

    Prepare for R2

--- manuscript/compile.sh
+++ manuscript/compile.sh
@@ -1,1 +1,2 @@
-latexdiff main-first-submission.tex main.tex > EVL3-25-0079-R1-diff.tex
+latexdiff main-first-submission.tex main-R1.tex > EVL3-25-0079-R1-diff.tex
+latexdiff main-R1.tex main.tex > EVL3-25-0079-R2-diff.tex
